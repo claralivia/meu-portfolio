5f71e6cf6198b9c63594a3d62d487cc369091e1f
claralmoura <claralivia2003@hotmail.com>
2026-04-24 19:23:58 -0300

    config: update vite configs for more performance

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('vue') || id.includes('vue-i18n') || id.includes('@vueuse')) {
+              return 'vendor-core'
+            }
+            if (id.includes('@heroicons')) {
+              return 'vendor-icons'
+            }
+            return 'vendor-others'
+          }
+        }
+      }
+    }
+  },
+  esbuild: {
+    drop: ['console', 'debugger'],
+  }
 })
